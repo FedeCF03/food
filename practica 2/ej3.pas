fec6b30f487bfc4ce941267bde3751ae782235d9
Federico Caltabiano <97410583+FedeCF03@users.noreply.github.com>
2024-03-25 23:07:35 -0300

    Add files via upload

--- practica 2/ej3.pas
+++ practica 2/ej3.pas
@@ -1,6 +1,7 @@
 
 Program ej3;
-valoralto = 9999;
+
+Const valoralto = 9999;
 
 Type 
   producto = Record
@@ -17,61 +18,62 @@ Type
   End;
   file_maestro = file Of producto;
   file_detalle = file Of producto_detalle;
+  array_detalle = array[1..30] Of file_detalle;
+  array_producto = array[1..30] Of producto;
+
+Procedure inicializar(Var archivo: array_detalle; Var array_prod: array_producto)
+
+Var i: integer;
+
+Var reg: producto_detalle
+         Begin
+           For ( i :=1; i To 30) Do
+             Begin
+               Assign(archivo[i], 'detalle'+IntToStr(i)+'.dat');
+               Reset(archivo[i]);
+               read(archivo[i], reg);
+               array_prod[i] := reg;
+             End;
+         End;
+
+
+Procedure minimo(Var archivo_detalle:array_detalle; Var min:producto_detalle; Var i:integer);
+
+Var j: integer;
+Begin
+  For ( j:=1 To 30) Do
+    Begin
+      If (producto_detalle[j].cod < min.cod) Then
+    End;
+End;
+End;
 
 Var 
   maestro: file_maestro;
-  detalle1: file_detalle;
-  detalle2: file_detalle;
-  detalle3: file_detalle;
   reg_m: producto;
-  reg_d1: producto_detalle;
-  reg_d2: producto_detalle;
-  reg_d3: producto_detalle;
+  array_det: array_detalle;
+  array_prod: array_producto;
   min: producto_detalle;
-Procedure minimo(Var r1:producto_detalle; Var r2:producto_detalle; Var r3:producto_detalle; Var min:producto_detalle);
-
-Begin
-  If (r1.cod<=r2.cod) And (r1.cod<=r3.cod) Then
-    Begin
-      min := r1;
-      leer(det1,r1)
-    End
-  Else If (r2.cod<=r3.cod) Then
+  i: integer
+     Begin
+       i := 1;
+       Assign(maestro, 'maestro.dat');
+       Reset(maestro);
+       inicializar(array_det, array_prod);
+       minimo(array_det,i,array_prod);
+       While (min.cod <> valoralto) Do
          Begin
-           min := r2;
-           leer(det2,r2)
-         End
-  Else
-    Begin
-      min := r3;
-      leer(det3,r3)
-    End;
-End
+           read(maestro, reg_m);
+           While (reg_m.cod <> min.cod) Do
+             read(maestro, reg_m);
+           While (reg_m.cod=array_prod[i].cod) Do
+             Begin
+               reg_m.stock_disp := reg_m.stock_disp - array_prod[i].cant_vendida;
+               minimo(array_det,i,array_prod);
+             End;
+           seek(maestro,filepos(maestro)-1);
+           write(maestro, reg_m);
+         End;
+       Close(maestro);
 
-Begin
-  Assign(maestro, 'maestro.dat');
-  Assign(detalle1, 'detalle1.dat');
-  Assign(detalle2, 'detalle2.dat');
-  Assign(detalle3, 'detalle3.dat');
-  Reset(maestro);
-  Reset(detalle1);
-  Reset(detalle2);
-  Reset(detalle3);
-  read(detalle1, reg_d1);
-  read(detalle2, reg_d2);
-  read(detalle3, reg_d3);
-  minimo(reg_d1, reg_d2, reg_d3,min);
-  While (min.cod <> valoralto) Do
-    Begin
-      read(maestro, reg_m);
-      While (reg_m.cod <> min.cod) Do
-        read(maestro, reg_m);
-      While (reg_m.cod=min.cod) Do
-        Begin
-          reg_m.stock_disp := reg_m.stock_disp - min.cant_vendida;
-          minimo(reg_d1, reg_d2, reg_d3, min);
-        End;
-      seek(maestro,filepos(maestro)-1);
-      write(maestro, reg_m);
-    End;
-End.
+     End.
